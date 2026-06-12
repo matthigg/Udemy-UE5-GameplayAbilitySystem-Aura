157256b0bba335b4e42b4bd59a28acd7a448670b
Matthew Higginbotham <116678780+matthigginbotham@users.noreply.github.com>
2026-06-12 18:00:56 -0400

    Video #100

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -70,17 +70,44 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 		FString("Amount of Mana regenerated every 1 second")
 	);
 	
-	
 	GameplayTags.Attributes_Secondary_MaxHealth = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("Attributes.Secondary.MaxHealth"), 
 		FString("Maximum amount of Health obtainable")
 	);
-	
 	
 	GameplayTags.Attributes_Secondary_MaxMana = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("Attributes.Secondary.MaxMana"), 
 		FString("Maximum amount of Mana obtainable")
 	);
 	
+	// Input
+	GameplayTags.InputTag_LMB = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.LMB"), 
+		FString("Input tag for Left Mouse Button")
+	);
+	
+	GameplayTags.InputTag_RMB = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.RMB"), 
+		FString("Input tag for Right Mouse Button")
+	);
+	
+	GameplayTags.InputTag_1 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.1"), 
+		FString("Input tag for 1 key")
+	);
+	
+	GameplayTags.InputTag_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.2"), 
+		FString("Input tag for 2 key")
+	);
+	
+	GameplayTags.InputTag_3 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.3"), 
+		FString("Input tag for 3 key")
+	);
+	
+	GameplayTags.InputTag_4 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.4"), 
+		FString("Input tag for 4 key")
+	);
 }
-
